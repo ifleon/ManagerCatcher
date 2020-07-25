c1b17dc9fd588754194a5b27d5c399754ed1e92c
Fredy Leon <fredy.leon@rappi.com>
2020-07-25 08:45:13 +0200

    first commit

---
+++ ManagerCatcher/ManagerCatcher.h
@@ -0,0 +1,19 @@
+//
+//  ManagerCatcher.h
+//  ManagerCatcher
+//
+//  Created by Fredy Leon on 25/07/2020.
+//  Copyright © 2020 Fredy Leon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ManagerCatcher.
+FOUNDATION_EXPORT double ManagerCatcherVersionNumber;
+
+//! Project version string for ManagerCatcher.
+FOUNDATION_EXPORT const unsigned char ManagerCatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ManagerCatcher/PublicHeader.h>
+
+
---
+++ ManagerCatcherExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManagerCatcherExample
+//
+//  Created by Fredy Leon on 25/07/2020.
+//  Copyright © 2020 Fredy Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
